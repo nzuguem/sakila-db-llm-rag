8873e1e5fead127f2ade97f13be4c1ceeb1e6179
Kevin NZUGUEM <nzuguemkevine@gmail.com>
2024-05-28 21:08:49 +0200

    🏷️ Bump version of Quarkus Langchain & Refactor Script run-sql.sh

--- scripts/run-sql.sh
+++ scripts/run-sql.sh
@@ -1,16 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
+: "${1?❗Error❗: Please provide an SQL query.}"
+
+SQL_QUERY=$1
 DOCKER_IMAGE="sakiladb/postgres"
 PGPASSWORD="p_ssW0rd"
 PGHOST="localhost"
 PGDATABASE="sakila"
 PGUSER="sakila"
-
-if [ -z "$1" ]; then
-    echo "Erreur : Veuillez fournir une requête SQL."
-    exit 1
-fi
-
-SQL_QUERY=$1
 
 docker run -i --net=host --rm --entrypoint "" -e PGPASSWORD=$PGPASSWORD $DOCKER_IMAGE psql -h $PGHOST -d $PGDATABASE -U $PGUSER -c "$SQL_QUERY"
